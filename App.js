7c4dc91e2b0d365dd7c11fc5bd673ffde790d709
Luchen2001 <luczhou@student.unimelb.edu.au>
2023-06-21 22:09:02 +1000

    week1 finished

--- App.js
+++ App.js
@@ -3,15 +3,14 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './WelcomeScreen';
-import MenuItem from './components/MenuItems';
+import LoginScreen from './LoginScreen';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <MenuItem/>
+        <LoginScreen />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
